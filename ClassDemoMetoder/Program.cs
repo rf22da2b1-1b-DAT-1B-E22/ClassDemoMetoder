f046b754b245564d3ffbd70e4d1d47aa53800507
Peter Levinsky <pele@easj.dk>
2022-09-22 13:30:36 +0200

    Add project files.

---
+++ ClassDemoMetoder/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int l = 90;
+int b = 10;
+
+int areal = Areal(l,b);
+
+Console.WriteLine($"l={l} b={b} arealet er = {areal}");
+
+
+
+
+ int Areal(int længde, int bredde)
+{
+        return længde * bredde;
+}
